7d896757ccac3837308e4e994a64d6f0ab664d13
Tim Rossiter <rosstimo@isu.edu>
2019-08-31 04:51:15 -0600

    remove beep

--- ClassRoomExamples/HelloClass.vb
+++ ClassRoomExamples/HelloClass.vb
@@ -3,7 +3,7 @@
     Sub Main()
         Console.WriteLine("Hello RCET0265")
         Console.ReadLine() 'This is a break point hack
-        Console.Beep(505, 500)
+        'Console.Beep(505, 500)
     End Sub
 
 End Module
