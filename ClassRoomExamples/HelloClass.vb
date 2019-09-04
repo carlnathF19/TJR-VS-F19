2dae0e9e191ca665b35ed44206c8a2145e13c382
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2019-09-04 08:14:11 -0600

    create Asg1-1

--- ClassRoomExamples/HelloClass.vb
+++
@@ -1,9 +0,0 @@
-﻿Module HelloClass
-
-    Sub Main()
-        Console.WriteLine("Hello RCET0265")
-        Console.ReadLine() 'This is a break point hack
-        'Console.Beep(505, 500)
-    End Sub
-
-End Module
