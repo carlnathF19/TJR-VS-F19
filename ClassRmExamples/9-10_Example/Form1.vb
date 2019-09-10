9ddafd779b4c3a509e80ee87fae9dd59dd66082b
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2019-09-10 12:22:49 -0600

    add working picturebox set image

--- ClassRmExamples/9-10_Example/Form1.vb
+++ ClassRmExamples/9-10_Example/Form1.vb
@@ -15,7 +15,13 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
         Button1.Enabled = False
 
-        PictureBox2.Image = "Resources\kitten-report.jpg"
+        'Setting an image like this works but it is hard coded to the file location of the current system
+        PictureBox2.Image = Image.FromFile("C:\Users\rosstimo\Documents\GitHub\TJR-VS-F19\ClassRmExamples\9-10_Example\Resources\kitten-report.jpg")
+
+        'useing the resource manager allows the IDE to bundle all the resources with the application for deployment
+        'learn more here: https://docs.microsoft.com/en-us/dotnet/visual-basic/language-reference/objects/my-resources-object
+        PictureBox2.Image = My.Resources.kitten_report
+
     End sub
 
 End Class
