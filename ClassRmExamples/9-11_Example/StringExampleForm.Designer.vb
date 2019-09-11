252b5d1dc52822e44e7eff2a2fc43f5cbcc6a23a
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2019-09-11 08:51:43 -0600

    create concat and display strings

---
+++ ClassRmExamples/9-11_Example/StringExampleForm.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class StringExampleForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FirstNameLabel = New System.Windows.Forms.Label()
+        Me.FirstNameTextBox = New System.Windows.Forms.TextBox()
+        Me.LastNamtTextBox = New System.Windows.Forms.TextBox()
+        Me.LastNameLabel = New System.Windows.Forms.Label()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.DisplayButton = New System.Windows.Forms.Button()
+        Me.DisplayLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'FirstNameLabel
+        '
+        Me.FirstNameLabel.AutoSize = True
+        Me.FirstNameLabel.Location = New System.Drawing.Point(135, 60)
+        Me.FirstNameLabel.Name = "FirstNameLabel"
+        Me.FirstNameLabel.Size = New System.Drawing.Size(40, 20)
+        Me.FirstNameLabel.TabIndex = 0
+        Me.FirstNameLabel.Text = "First"
+        '
+        'FirstNameTextBox
+        '
+        Me.FirstNameTextBox.Location = New System.Drawing.Point(198, 54)
+        Me.FirstNameTextBox.Name = "FirstNameTextBox"
+        Me.FirstNameTextBox.Size = New System.Drawing.Size(176, 26)
+        Me.FirstNameTextBox.TabIndex = 1
+        '
+        'LastNamtTextBox
+        '
+        Me.LastNamtTextBox.Location = New System.Drawing.Point(198, 95)
+        Me.LastNamtTextBox.Name = "LastNamtTextBox"
+        Me.LastNamtTextBox.Size = New System.Drawing.Size(176, 26)
+        Me.LastNamtTextBox.TabIndex = 2
+        '
+        'LastNameLabel
+        '
+        Me.LastNameLabel.AutoSize = True
+        Me.LastNameLabel.Location = New System.Drawing.Point(135, 95)
+        Me.LastNameLabel.Name = "LastNameLabel"
+        Me.LastNameLabel.Size = New System.Drawing.Size(40, 20)
+        Me.LastNameLabel.TabIndex = 3
+        Me.LastNameLabel.Text = "Last"
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(613, 355)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(156, 59)
+        Me.ExitButton.TabIndex = 4
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(451, 355)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(156, 60)
+        Me.ClearButton.TabIndex = 5
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'DisplayButton
+        '
+        Me.DisplayButton.Location = New System.Drawing.Point(289, 355)
+        Me.DisplayButton.Name = "DisplayButton"
+        Me.DisplayButton.Size = New System.Drawing.Size(156, 59)
+        Me.DisplayButton.TabIndex = 6
+        Me.DisplayButton.Text = "Show"
+        Me.DisplayButton.UseVisualStyleBackColor = True
+        '
+        'DisplayLabel
+        '
+        Me.DisplayLabel.Location = New System.Drawing.Point(194, 164)
+        Me.DisplayLabel.Name = "DisplayLabel"
+        Me.DisplayLabel.Size = New System.Drawing.Size(461, 121)
+        Me.DisplayLabel.TabIndex = 7
+        '
+        'StringExampleForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DisplayLabel)
+        Me.Controls.Add(Me.DisplayButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.LastNameLabel)
+        Me.Controls.Add(Me.LastNamtTextBox)
+        Me.Controls.Add(Me.FirstNameTextBox)
+        Me.Controls.Add(Me.FirstNameLabel)
+        Me.Name = "StringExampleForm"
+        Me.Text = "Concatinate Text"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents FirstNameLabel As Label
+    Friend WithEvents FirstNameTextBox As TextBox
+    Friend WithEvents LastNamtTextBox As TextBox
+    Friend WithEvents LastNameLabel As Label
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents DisplayButton As Button
+    Friend WithEvents DisplayLabel As Label
+End Class
---
+++ ClassRmExamples/9-11_Example/StringExampleForm.vb
@@ -0,0 +1,15 @@
+﻿Public Class StringExampleForm
+    Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
+        'concatinate first and last name then display properly
+        DisplayLabel.Text = FirstNameTextBox.Text & " " & LastNamtTextBox.Text
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        DisplayLabel.Text = ""
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+End Class
