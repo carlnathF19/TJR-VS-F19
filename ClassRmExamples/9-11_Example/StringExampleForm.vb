5a7bf74c851f951147ca244be911da3dbc688ac7
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2019-09-12 09:29:44 -0600

    add long string example

--- ClassRmExamples/9-11_Example/StringExampleForm.vb
+++ ClassRmExamples/9-11_Example/StringExampleForm.vb
@@ -1,7 +1,15 @@
-﻿Public Class StringExampleForm
+﻿'Tim Rossiter
+'RCET0265
+'Example
+'String Form Example Program
+'Repo url here
+
+
+Public Class StringExampleForm
     Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
         'concatinate first and last name then display properly
         DisplayLabel.Text = FirstNameTextBox.Text & " " & LastNamtTextBox.Text
+        BigString()
     End Sub
 
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
@@ -11,5 +19,24 @@
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
+
+    Sub BigString()
+        Dim longString As String
+        longString =
+             " " & "This is a very long string..." & vbNewLine _
+            & " " & "This is a very long string..." & vbNewLine _
+            & " " & "This is a very long string..." & vbNewLine _
+            & " " & "This is a very long string..." & vbNewLine _
+            & " " & "This is a very long string..." & vbNewLine _
+            & " " & "This is a very long string..." & vbNewLine _
+            & " " & "This is a very long string..." & vbNewLine _
+            & " " & "This is a very long string..." & vbNewLine _
+            & " " & "This is a very long string..." & vbNewLine _
+            & " " & "This is a very long string..." & vbNewLine _
+            & " " & "This is a very long string..."
+        Console.WriteLine(longString)
+
+    End Sub
+
 
 End Class
