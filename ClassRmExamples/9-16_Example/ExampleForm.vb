4c7cc14b94dd109ee33071ee55b631ae6bdfc07e
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2019-09-16 08:51:22 -0600

    create constant example

---
+++ ClassRmExamples/9-16_Example/ExampleForm.vb
@@ -0,0 +1,40 @@
+﻿Option Explicit On
+Option Strict On
+Public Class Form
+
+    'PascalCase FirstLetterOfEachWordCapital no spaces
+    'Objects
+
+    'camelCase
+    'variablesUseCamelCase
+
+    'ALLCAPS
+    'CONSTANTS
+
+    'snake_case
+    'used in python havn't seen in .NET
+
+    Sub VariablesAndConstants()
+        'Variables can be changed
+        Dim someNumber As Integer
+        Dim result As Integer
+
+        'Constants can not change
+        Const SOMECONSTANT = 7D
+
+        someNumber = 5
+        'Console.WriteLine(someNumber)
+        'someNumber = 6
+        'Console.WriteLine(someNumber)
+
+        'result = someNumber + Convert.ToInt32(SOMECONSTANT)
+        result = someNumber + CInt(SOMECONSTANT)
+
+
+        Console.WriteLine()
+    End Sub
+
+    Private Sub RunButton_Click(sender As Object, e As EventArgs) Handles RunButton.Click
+        VariablesAndConstants()
+    End Sub
+End Class
