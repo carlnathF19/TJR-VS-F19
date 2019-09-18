8b3708474255ff51349d85c8206d254501dd0bd7
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2019-09-18 08:55:07 -0600

    add sample data

--- ClassRmExamples/9-18_Example/ExampleForm.vb
+++ ClassRmExamples/9-18_Example/ExampleForm.vb
@@ -7,6 +7,8 @@ Public Class ExampleForm
         Dim errorMessage As String
         errorMessage = ""
 
+        LoadTestData() 'load sample data remove for production
+
         'validate user input in reverse tabstop order. first tab stop with an error will be left selected.
         Try
             'try somthing that might throw an exception
@@ -37,4 +39,32 @@ Public Class ExampleForm
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
         Me.Close()
     End Sub
+
+    Private Sub LoadTestData()
+        Dim sample As Integer
+        'Static variables will fix this
+        For sample = 1 To 4 Step 1
+            Select Case sample
+                Case = 1
+                    TextBox1.Text = "5"
+                    TextBox2.Text = "6"
+                    Exit For
+                Case = 2
+                    TextBox1.Text = "bad"
+                    TextBox2.Text = "6"
+                    Exit For
+                Case = 3
+                    TextBox1.Text = "5"
+                    TextBox2.Text = "bad"
+                    Exit For
+                Case = 4
+                    TextBox1.Text = "bad"
+                    TextBox2.Text = "bad"
+                    Exit For
+                Case Else
+                    'some unexpected thing
+            End Select
+        Next sample
+    End Sub
+
 End Class
